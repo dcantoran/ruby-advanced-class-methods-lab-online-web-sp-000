c3f45e6e3cf214be819b954980c3dbac49e1c4b6
David Cantoran <davidcantoran36@gmail.com>
2019-12-15 23:22:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,8 +12,8 @@ class Song
   
   def self.new_by_name(song_name)
     song = self.new 
-    song.name = song_name
-    binding.pry
+    song_name = song.name
+    # binding.pry
   end 
   
   def self.create_by_name(name_prop)
