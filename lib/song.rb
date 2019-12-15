a668fa4731ea0dd5166261864589fca6c5a37050
David Cantoran <davidcantoran36@gmail.com>
2019-12-15 23:53:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,12 +32,7 @@ class Song
   end 
   
   def self.find_or_create_by_name(name_str)
-    if !self.find_by_name(name_str)
-      create_by_name(name_str)
-    else 
-      self.find_by_name(name_str)
-    end 
-    
+    create_by_name(name_str) || find_by_name(name_str)
     
   end 
 
