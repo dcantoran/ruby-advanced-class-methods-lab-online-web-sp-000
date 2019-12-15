72180e835dd9abc4892bdeb111da2946c5ac9f35
David Cantoran <davidcantoran36@gmail.com>
2019-12-15 23:35:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,6 +21,14 @@ class Song
     song.name = song_name
     song
   end 
+  
+  def self.find_by_name(song_name)
+    @@all.map do |song|
+      if song.name == song_name
+        song
+      end 
+    end 
+  end 
 
   def self.all
     @@all
